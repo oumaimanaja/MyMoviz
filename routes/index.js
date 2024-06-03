70cb2e8e40f832ff243433c03da64557f3ff4848
oumaima <oumaima.naja@gmail.com>
2024-06-03 21:27:47 +0200

    change .env file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var OWN_API_KEY = process.env.OWN_API_KEY;
+var OWM_API_KEY = process.env.OWN_API_KEY;
 
 router.get("/movies", (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWN_API_KEY}`)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
     .then((reponse) => reponse.json())
     .then((data) => res.json({ movies: data.results }));
 });
--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = "https://my-moviz-backend-dusky.vercel.app/";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = "https://my-moviz-frontend-alpha-one.vercel.app/";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
