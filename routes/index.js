348bbf862e1c64796cb335a3373bd75983f38b10
oumaima <oumaima.naja@gmail.com>
2024-06-04 22:06:27 +0200

    API key injected in file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var OWM_API_KEY = process.env.OWN_API_KEY;
+//var OWM_API_KEY = process.env.OWN_API_KEY;
+OWM_API_KEY = "6e7160850b09d7005ce99246185863a8";
 
 router.get("/movies", (req, res) => {
   fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
